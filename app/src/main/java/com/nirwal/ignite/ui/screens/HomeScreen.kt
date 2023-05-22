c222ce7db59592f3fbc34c412b5ff9e7e1e3cf06
NIRWAL <nirwal@live.com>
2023-05-22 22:59:23 +0530

    Initial commit

--- app/src/main/java/com/nirwal/ignite/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/nirwal/ignite/ui/screens/HomeScreen.kt
@@ -1,4 +1,56 @@
 package com.nirwal.ignite.ui.screens
 
-class HomeScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.nirwal.ignite.R
+
+@Composable
+fun HomeScreen() {
+    LazyVerticalGrid(
+        modifier = Modifier.fillMaxSize(),
+        columns = GridCells.Adaptive(100.dp)
+    ){
+       repeat(20){
+
+           item {
+               WallpaperItem()
+           }
+       }
+    }
 }
+
+@Composable
+fun WallpaperItem() {
+    Column {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("https://images.pexels.com/photos/2014422/pexels-photo-2014422.jpeg?auto=compress&cs=tinysrgb&h=130")
+                .crossfade(true)
+                .build(),
+            placeholder = painterResource(R.drawable.fire_skul),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.clip(CircleShape)
+        )
+        Text(text = "Sample Image")
+    }
+}
+
+
