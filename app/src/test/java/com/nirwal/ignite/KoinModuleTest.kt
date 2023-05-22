344c4b04663dd66605550fccc980993bd7af9921
NIRWAL <nirwal@live.com>
2023-05-22 22:59:23 +0530

    Initial commit

--- app/src/test/java/com/nirwal/ignite/KoinModuleTest.kt
+++ app/src/test/java/com/nirwal/ignite/KoinModuleTest.kt
@@ -2,6 +2,7 @@ package com.nirwal.ignite
 
 import com.nirwal.ignite.di.appModule
 import org.koin.test.KoinTest
+import org.koin.test.verify.verify
 import kotlin.test.Test
 
 class KoinModuleTest:KoinTest {
